b26641195ab1b383c0f054834f43c7e7ff680196
ShadiK1999 <123907887+ShadiK1999@users.noreply.github.com>
2023-02-08 16:28:25 +1100

    Create playRound function     - accept player choice and converts to lower changes     - compare player choice with computer choice and       return the winner via switch statement

--- script.js
+++ script.js
@@ -4,10 +4,52 @@ function getComputerChoice(){
     return choice
 }
 
-
-//function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
+    //makes selection input not case sensitive 
+    pSelection = playerSelection.toLocaleLowerCase();
     
-//}
+    switch(pSelection) {
+        case "rock":
+            if (computerSelection === "scissors"){
+                    console.log("You Win! Rock beats Scissors");
+                    return "player";
+            } else if(computerSelection === "paper"){
+                console.log("You Lose! Paper beats Rock");
+                return "comp";
+            } else {
+                console.log("DRAW!");
+                return "draw";
+            }
+        break;
+        case "paper":
+            if (computerSelection === "scissors"){
+                    console.log("You Lose! Scissors beats Paper");
+                    return "comp";
+            } else if(computerSelection === "rock"){
+                console.log("You Win! Paper beats Rock");
+                return "player";
+            } else {
+                console.log("DRAW!");
+                return "draw";
+            }
+        break;
+        case "scissors":
+            if (computerSelection === "rock"){
+                    console.log("You Lose! Rock beats Scissors");
+                    return "comp";
+            } else if(computerSelection === "paper"){
+                console.log("You Win! Scissors beats Paper");
+                return "player";
+            } else {
+                console.log("DRAW!");
+                return "draw";
+            }
+        break;
+        default:
+            console.log("Invaid choice");
+    }
+
+}
 
 //function game(){
 
